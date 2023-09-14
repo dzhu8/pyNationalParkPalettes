ffff6ebb76c3d47e90639467a4c805dd776c8480
Daniel Y. Zhu <danielyumengzhu@gmail.com>
2023-09-14 11:16:48 -0400

    Initial commit

---
+++ palettes.py
@@ -0,0 +1,88 @@
+NatParksPalettes = {
+    "Acadia": {
+        'colors': ["#212E52", "#444E7E", "#8087AA", "#B7ABBC", "#F9ECE8", "#FCC893", "#FEB424", "#FD8700", "#D8511D"],
+        'indices': [0, 1, 2, 3, 4, 5, 6, 7, 8],
+        'colorblind': True
+    },
+    "Arches": {
+        'colors': ["#1A3D82", "#0C62AF", "#4499F5", "#8FCAFD", "#F2F2F2", "#F0AC7D", "#CD622E", "#B14311", "#832B0F"],
+        'indices': [0, 1, 2, 3, 4, 5, 6, 7, 8],
+        'colorblind': True
+    },
+    "Arches2": {
+        'colors': ["#3A1F46", "#7F4B89", "#B46DB3", "#E3A5D6", "#F3DAE4"],
+        'indices': [0, 1, 2, 3, 4],
+        'colorblind': True
+    },
+    "Banff": {
+        'colors': ["#006475", "#00A1B7", "#55CFD8", "#586028", "#898928", "#616571", "#9DA7BF"],
+        'indices': [1, 4, 0, 5, 2, 6, 3],
+        'colorblind': False
+    },
+    "BryceCanyon": {
+        'colors': ["#882314", "#C0532B", "#CF932C", "#674D53", "#8C86A0", "#724438", "#D5AB85"],
+        'indices': [0, 4, 1, 6, 3, 2, 5],
+        'colorblind': False
+    },
+    "CapitolReef": {
+        'colors': ["#291919", "#532A34", "#7C5467", "#878195", "#AEB2B7", "#D4D9DD"],
+        'indices': [0, 1, 2, 3, 4, 5],
+        'colorblind': True
+    },
+    "Charmonix": {
+        'colors': ["#008FF8", "#B6AA0D", "#E2C2A2", "#E23B0E", "#F2C621", "#196689"],
+        'indices': [0, 1, 2, 3, 4, 5],
+        'colorblind': False
+    },
+    "CraterLake": {
+        'colors': ["#1D4A79", "#794C23", "#6B7444", "#6089B5", "#BF9785", "#275E4D", "#807B7F"],
+        'indices': [0, 1, 2, 3, 4, 5, 6],
+        'colorblind': False
+    },
+    "Cuyahoga": {
+        'colors': ["#E07529", "#FAAE32", "#7F7991", "#A84A00", "#5D4F36", "#B39085"],
+        'indices': [0, 1, 2, 3, 4, 5],
+        'colorblind': True
+    },
+    "DeathValley": {
+        'colors': ["#8C2B0E", "#C5692D", "#FEB359", "#132F5B", "#435F90", "#68434E", "#B47E83"],
+        'indices': [0, 4, 6, 1, 5, 2, 3],
+        'colorblind': True,
+    },
+    "Denali": {
+        'colors': ["#20223E", "#3F3F7B", "#278192", "#00B089", "#2EEA8C", "#8FF7BD"],
+        'indices': [0, 1, 2, 3, 4, 5],
+        'colorblind': False,
+    },
+    "Everglades": {
+        'colors': ["#345023", "#596C0B", "#83A102", "#003B68", "#426F86", "#7A712F"],
+        'indices': [2, 3, 0, 5, 4, 1],
+        'colorblind': False,
+    },
+    "Glacier": {
+        'colors': ["#01353D", "#088096", "#58B3C7", "#7AD4E4", "#B8FCFC"],
+        'indices': [0, 1, 2, 3, 4],
+        'colorblind': True,
+    },
+    "GrandCanyon": {
+        'colors': ["#521E0F", "#9C593E", "#DDA569", "#3F4330", "#8E7E3C", "#2A4866", "#6592B0"],
+        'indices': [1, 5, 2, 3, 6, 0, 4],
+        'colorblind': False,
+    },
+    "Halekala": {
+        'colors': ["#722710", "#A3844D", "#675243", "#A85017", "#838BAA"],
+        'indices': [0, 1, 2, 3, 4],
+        'colorblind': True,
+    },
+    "IguazuFalls": {
+        'colors': ["#415521", "#97AD3D", "#4C3425", "#7F6552", "#5A8093", "#9FBAD3"],
+        'indices': [0, 1, 2, 3, 4, 5],
+        'colorblind': False,
+    },
+    "KingsCanyon": {
+        'colors': ["#613921", "#A77652", "#F2C27B", "#AAC9ED", "#44637D", "#8E949F"],
+        'indices': [0, 4, 5, 2, 1, 3],
+        'colorblind': True,
+    },
+    
+}
